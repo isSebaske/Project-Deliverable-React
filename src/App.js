3956b76f6f871556e8b5da182d9b0499882a9d3c
Isaac Sebaske <issebaske@gmail.com>
2024-09-02 00:26:12 -0600

    updated App class to Store for the assignment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ProductsList from "./com/productsList";
 import { getProducts } from "./data/ItemsData";
 
-class App extends Component {
+class Store extends Component {
 
   state = { products: getProducts() } 
 
@@ -13,4 +13,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Store;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Store from './App';
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Store />
   </React.StrictMode>
 );
 
